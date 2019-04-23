1d2a2aac98e9003f8949339b6746a4845fea7bbe
Aelousdp <806267856@qq.com>
2019-04-23 11:34:19 +0800

    修改IP地址

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const urlMap = {
   development: '/',
-  production: 'http://192.168.1.137/sell/'
+  production: 'http://192.168.1.117/sell/'
 }
 const basUrl = urlMap[process.env.NODE_ENV]
 const ERR_OK = 0
